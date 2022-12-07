2ba6685827f56b4c4527eb2c9152d515338e5ff8
NabilMunir45 <nabilbutt45@gmail.com>
2022-12-07 16:08:03 +0500

    Hello git

---
+++ utils/data.js
@@ -0,0 +1,42 @@
+const items = [
+  {
+    name: "Fruits",
+    price: 1,
+    category: "FRUITS",
+    image:
+      "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4",
+  },
+  {
+    name: "Vegetables",
+    price: 2,
+    category: "FRUITS",
+    image: "https://cdn.britannica.com/17/196817-050-6A15DAC3/vegetables.jpg",
+  },
+  {
+    name: "Oil",
+    price: 10,
+    category: "COOKING OIL",
+    image: "https://www.nicecookingoil.com.pk/newimages/nice-cooking-new-4.jpg",
+  },
+  {
+    name: "Banaspati Ghee",
+    price: 5,
+    category: "COOKING OIL",
+    image: "http://www.taazaproducts.com/images/taazabanaspati.jpg",
+  },
+  {
+    name: "Flour",
+    price: 7,
+    category: "RICE AND FLOUR",
+    image:
+      "https://www.thespruceeats.com/thmb/2fdZNcOtnWsiZrhMwuRaOEFSBDk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wheat-and-flour-g2k-57bb87385f9b58cdfd6ab04b.jpg",
+  },
+  {
+    name: "Rice",
+    price: 5,
+    category: "RICE AND FLOUR",
+    image:
+      "https://www.world-grain.com/ext/resources/Article-Images/2020/12/GMR_Rice_AdobeStock_64819529_E_Nov.jpg?t=1609338918&width=1080",
+  },
+];
+module.exports = items;
